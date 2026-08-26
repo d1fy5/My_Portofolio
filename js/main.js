34ce4f2382e7098004780e4d4ac63e7a7ed19e06
Muhammad Daffa Alfarisi <daffalfari1108@gmail.com>
2026-08-26 12:15:51 +0700

    Add NutriMate hover video mockup

--- js/main.js
+++ js/main.js
@@ -71,4 +71,31 @@
     { rootMargin: "-40% 0px -55% 0px" }
   );
   sections.forEach((section) => sectionObserver.observe(section));
+
+  document.querySelectorAll(".project-preview--video").forEach((preview) => {
+    const video = preview.querySelector(".project-video");
+    if (!video) return;
+
+    const playVideo = () => {
+      video.currentTime = 0;
+      video.play().catch(() => {});
+      preview.classList.add("is-playing");
+    };
+
+    const stopVideo = () => {
+      video.pause();
+      video.currentTime = 0;
+      preview.classList.remove("is-playing");
+    };
+
+    preview.addEventListener("mouseenter", playVideo);
+    preview.addEventListener("mouseleave", stopVideo);
+
+    preview.addEventListener("focusin", playVideo);
+    preview.addEventListener("focusout", stopVideo);
+
+    video.addEventListener("ended", () => {
+      video.currentTime = 0;
+    });
+  });
 })();
